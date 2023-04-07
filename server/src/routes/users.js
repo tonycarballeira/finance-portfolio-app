b6aa2b396e56352e58d0db49384ca51ea549f81b
tonycarballeira <tonycarballeira@gmail.com>
2023-04-07 17:48:43 -0400

    create user api end point

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -10,7 +10,16 @@ router.post("/register", async (req, res) => {
 
     const user = await UserModel.findOne({username: username});
 
-    res.json(user);
+    if (user) {
+        return res.json({message: "User Already Exists"});  
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new UserModel({username, password: hashedPassword});
+    await newUser.save();
+
+    res.json({message: "User registered successfully"});
 });
 
 router.post("/login");
