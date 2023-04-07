a97183cb145db79d7e48a3f28f66e84f45a60664
tonycarballeira <tonycarballeira@gmail.com>
2023-04-07 17:30:08 -0400

    started api endpoint for user creation

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,13 @@
     import cors from "cors";
     import mongoose from "mongoose";
 
+    import { userRouter } from "./routes/users.js";
+
     const app = express();
 
     app.use(express.json());
     app.use(cors());
+    app.use("/auth", userRouter);
 
     mongoose.connect(
         "mongodb+srv://tonycarballeira:theduke23@financialportfolioapp.l8khwfp.mongodb.net/FinancialPortfolioApp?retryWrites=true&w=majority"
