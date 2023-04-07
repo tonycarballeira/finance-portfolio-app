4e223920dd106750be9150fb38cb54bfd8ba5c39
tonycarballeira <tonycarballeira@gmail.com>
2023-04-07 17:41:05 -0400

    fixed bug in user model file

--- server/src/models/Users.js
+++ server/src/models/Users.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.schema({
+const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true},
     password: { type: String, required: true}
 }); 
