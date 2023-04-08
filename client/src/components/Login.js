6ed78abccf661b5bd030d2652e5f876097c45d84
tonycarballeira <tonycarballeira@gmail.com>
2023-04-08 15:35:38 -0400

    login user api request and navbar logout button

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,36 @@
 import React, { useState } from 'react'
+import axios from 'axios';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    const [_, setCookies] = useCookies(["access_token"]);
+
+    const navigate = useNavigate();
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await axios.post("http://localhost:3001/auth/login", 
+            { username, password });
+
+            setCookies("access_token", response.data.token);
+            window.localStorage.setItem("userID", response.data.userID);
+            navigate("/");
+
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
   return (
     <div className='auth-container'>
-        <form>
+        <form onSubmit={onSubmit}>
             <h2>Login</h2>
             <div className='form-group'>
                 <label htmlFor='username'> Username:</label>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+
+  const [cookies, setCookies] = useCookies(["access_token"]);  
+  const navigate = useNavigate();
+
+  const logout = () => {
+    setCookies("access_token", "");
+    window.localStorage.removeItem("userID");
+    navigate("/auth");
+  };
+
   return (
     <div className='navbar'>
       <Link to="/">Home</Link>
-      <Link to="/auth">Login</Link>
+      {!cookies.access_token ? (<Link to="/auth">Login</Link>) : <button onClick={logout}>Logout</button>}
+      
     </div>
   )
 }
