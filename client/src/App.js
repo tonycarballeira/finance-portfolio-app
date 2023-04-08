849ce8328e690a6317bdcb57381ba32b758bb6e2
tonycarballeira <tonycarballeira@gmail.com>
2023-04-08 14:13:14 -0400

    navbar using router links imported to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Auth from './pages/auth';
+import Navbar from './compoments/navbar';
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router>  
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
---
+++ client/src/compoments/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <Link to="/">Home</Link>
+      <Link to="/auth">Login</Link>
+    </div>
+  )
+}
+
+export default Navbar
