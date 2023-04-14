9c7f0cda29abbd2aeb0bdb8e386285bb21fb2cf3
tonycarballeira <tonycarballeira@gmail.com>
2023-04-14 15:40:07 -0400

    fetch historical data finnhub api

--- client/src/api/stock-api.js
+++ client/src/api/stock-api.js
@@ -11,3 +11,41 @@ export const searchSymbols = async (query) => {
 
     return await response.json();
 }
+
+export const fetchStockDetails = async (stockSymbol) => {
+    const url = `${basePath}/stock/profile2?symbol=${stockSymbol}&token=${process.env.REACT_APP_API_KEY}`;
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        const message = `An error has occured: ${response.status}`
+        throw new Error(message);
+    }
+
+    return await response.json();
+};
+
+export const fetchQuote = async (stockSymbol) => {
+    const url = `${basePath}/quote?symbol=${stockSymbol}&token=${process.env.REACT_APP_API_KEY}`;
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        const message = `An error has occured: ${response.status}`
+        throw new Error(message);
+    }
+
+    return await response.json();
+};
+
+export const fetchHistoricalData = async (stockSymbol, resolution, from, to) => {
+    const url = `${basePath}/stock/candle?symbol=${stockSymbol}&resolution=${resolutionn}
+    &from=${from}&to=${to}&token=${process.env.REACT_APP_API_KEY}`;
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        const message = `An error has occured: ${response.status}`
+        throw new Error(message);
+    }
+
+    return await response.json();
+
+};
