e8a0c32a6a7173ab050f5056c1fb3575ce26d0c3
tonycarballeira <tonycarballeira@gmail.com>
2023-04-14 14:53:04 -0400

    modified chart

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -4,6 +4,8 @@ import { mockHistoricalData } from '../constants/mock'
 import { convertUnixTimestampToDate } from '../helpers/date-helper';
 import {Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
 import Card from './card/Card';
+import ChartFilter from './ChartFilter';
+import { chartConfig } from '../constants/config';  
 
 const Chart = () => {
 
@@ -21,8 +23,20 @@ const Chart = () => {
 
   return (
     <Card>
+        <ul className='flex top-2 right-2 z-40'>
+            {Object.keys(chartConfig).map((item) => {
+                return (
+                <li key={item}>
+                    <ChartFilter text={item} active={filter === item} onCLick={() => {
+                        setFilter(item);
+                    }}/>
+                </li>)
+            })}
+        </ul>
         <ResponsiveContainer>
-            <AreaChart data={formatData(data)}>
+            <AreaChart data={formatData(data)} margin={{
+                    top: 20, right: 20, bottom: 50, left: 20,
+                }}>
                 <defs>
                     <linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
                     <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
@@ -32,7 +46,7 @@ const Chart = () => {
                 <Area type="monotone" dataKey="value" stroke="gray"
                 fillOpacity={1} strokeWidth={0.5} fill="url(#chartColor)"/>
                 <Tooltip />
-                <XAxis data={"date"}/>
+                <XAxis dataKey="date" />
                 <YAxis domain={["dataMin", "dataMax"]}/>
             </AreaChart>
         </ResponsiveContainer>
