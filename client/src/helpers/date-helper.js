30fa279c44fbe64184cf542228659597332e13fe
tonycarballeira <tonycarballeira@gmail.com>
2023-04-14 13:59:34 -0400

    function to create dates for chart

--- client/src/helpers/date-helper.js
+++ client/src/helpers/date-helper.js
@@ -1,3 +1,13 @@
 export const convertDateToUnixTimestamp = (date) => {
     return Math.floor(date.getTime() / 1000); 
 };
+
+
+export const convertUnixTimestampToDate = (unixTimestamp) => {
+    const milliseconds = unixTimestamp * 1000;
+    return new Date(milliseconds).toLocaleDateString();
+};
+
+export const createDate = (date, days, weeks, months, years) => {
+
+};
